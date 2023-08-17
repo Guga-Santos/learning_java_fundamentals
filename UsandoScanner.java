5fc519f1855a92d8d97aa994dce959671f5e36e0
GugaSantos <guga.santos.guga@gmail.com>
2023-08-17 15:31:49 -0300

    feat: complemento

--- UsandoScanner.java
+++ UsandoScanner.java
@@ -10,6 +10,20 @@ class UsandoScanner {
     System.out.print("Qual seu maior desejo?: ");
     String desejo = scanner.next();
     System.out.println("Você quer " + desejo + " porque?");
+    System.out.println("Quanto você ganha por mês? ");
+    String salario = scanner.next();
+    System.out.println("Quanto você precisa pra realizar seu sonho?");
+    String sonho = scanner.next();
+
+    int sal = Integer.parseInt(salario);
+    int son = Integer.parseInt(sonho);
+
+    int resultado = son / sal;
+
+    System.out.println("Não desanime! Você só precisa trabalhar mais " + resultado + " meses para conquistar tudo que mais quer!");
+
+
+
 
 
 
